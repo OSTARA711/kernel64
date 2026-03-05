708cbdabf5c833983e8729c204c188e8791dcc58
Ferrex2 <xaviferrer2011@gmail.com>
2026-03-05 19:13:38 +0100

    I put the essencial in kernel.c

---
+++ src/kernel.c
@@ -0,0 +1,17 @@
+#include<stdint.h>
+#include<stddef.h>
+
+
+#define VGA_ADDRESS 0xB8000
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+void putchar(char c){
+    
+
+}
+
+
+void kmain(void) {
+
+}
